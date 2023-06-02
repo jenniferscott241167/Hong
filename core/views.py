faa02b02308e619f822f025c0e5161370ad506e1
daniel <scarlitecomedy@gmail.com>
2023-06-02 18:13:29 +0100

    added changes

--- core/views.py
+++ core/views.py
@@ -32,7 +32,7 @@ def send_withdrawal_verification_mail(email,token):
     send_mail(
     "Withdrawal Request",
     f'You have requested a Withdrawal from your account in coinface.\nif these was not you kindly ignore this mail or contact support.\nVerification Token: {token}',
-    from_email="support@coinface.us",
+    from_email="support@naxtrust.com",
     recipient_list=[email],
     fail_silently=False)
 def generate_token():
