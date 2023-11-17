03640519ed24b1ee6aef3866b4ebf121aa24e387
NaxTrust <104193867+NaxTrust@users.noreply.github.com>
2023-11-17 08:55:44 +0100

    Update settings.py

--- ForexPro/settings.py
+++ ForexPro/settings.py
@@ -158,11 +158,11 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST_USER = "reply.naxtrust.com@gmail.com"
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = "support@naxtrust.com"
+    EMAIL_HOST = 'mail.privateemail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_PASSWORD = "lfjd ssqy fdxt soih"
+    EMAIL_HOST_PASSWORD = "241167emeKA"
 
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
